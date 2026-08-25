d0c052add5734b5ff4811ecea44a913a55a5d8df
jtgeorge1970-tech <jtgeorge1970@gmail.com>
2026-08-24 18:13:36 -0600

    Add Next.js application layout

---
+++ app/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'BTA ImportYeti Explorer',
+  description: 'Secure server-side importer data explorer'
+};
+
+export default function RootLayout({ children }) {
+  return <html lang="en"><body style={{margin:0}}>{children}</body></html>;
+}
